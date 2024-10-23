82f5f1ae4b576e792422541e798f703fc5efce42
SolanaSpinDev3 <dev_sol_99@proton.me>
2024-10-23 19:14:55 +0300

    glue video dirty version

--- app/components/WheelContainer.tsx
+++ app/components/WheelContainer.tsx
@@ -48,34 +48,20 @@ const videoSources = [
   "/videos/result/S_W_Separate_Wood_Result_X5A.mp4",
   "/videos/result/S_W_Separate_Wood_Result_X5B.mp4"
 ];
+const wheelPositions = 14;
 
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const WheelContainer: React.FC = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const videoRefs = useRef([]); // Array of references for video elements
-  const [videoId, setVideoId] = useState(1); // Tracks which video is playing
+  const [videoId, setVideoId] = useState(1);
   const [balance, setBalance] = useState(10000);
   const [isLoading, setIsLoading] = useState(true);
   const [videoBlobs, setVideoBlobs] = useState([]); // Store preloaded video blob URLs
-
-  const handlePlayVideo = () => {
-    // Set the new video number
-    let newVideoId = Math.floor(Math.random() * videoSources.length) + 1;
-
-    // Prevent playing the same video again this is optional
-    while (newVideoId === videoId) {
-      newVideoId = Math.floor(Math.random() * videoSources.length) + 1;
-    }
-
-    // Stop the currently playing video (if any)
-    if (videoRefs.current[videoId - 1]) {
-      videoRefs.current[videoId - 1].pause();
-      videoRefs.current[videoId - 1].currentTime = 0;
-    }
-
-    setVideoId(newVideoId);
-    setIsPlaying(true);
-  };
+  const [firstSpin, setFirstSpin] = useState(true);
 
   // Preload videos into blob URLs
   useEffect(() => {
@@ -94,7 +80,6 @@ const WheelContainer: React.FC = () => {
 
     fetchVideos();
   }, []);
-
   useEffect(() => {
     if (isPlaying && videoRefs.current[videoId - 1]) {
       // Play the new video
@@ -102,17 +87,88 @@ const WheelContainer: React.FC = () => {
     }
   }, [videoId, isPlaying]);
 
+  const handlePlayVideo = () => {
+    setBalance(balance + 1);
+    if (firstSpin) {
+      console.log('first spin XXXXX')
+      setVideoId(1);
+      setIsPlaying(true);
+
+      return;
+    }
+    if (videoId > 2 * wheelPositions) {
+      console.log('video id should be section3 if returns after a result, means no first spin, YYYYY ', videoId);
+      setVideoId(videoId - 2 * wheelPositions);
+      setIsPlaying(true);
+    } else {
+      console.log('we never get here')
+      alert('abracadabra')
+      console.log('ZZZZZ')
+      const newVideoId = getRandomNumber(15, 28);
+      // Stop the currently playing video (if any)
+      if (videoRefs.current[videoId - 1]) {
+        videoRefs.current[videoId - 1].pause();
+        videoRefs.current[videoId - 1].currentTime = 0;
+      }
+
+      setVideoId(newVideoId);
+      setIsPlaying(true);
+    }
+  };
+
+
   const handleVideoEnd = () => {
     // The video naturally stays on the last frame when ended, no action needed.
     setIsPlaying(false);
+
     console.log("Video ended, stays at the last frame:", videoId);
+    if (videoId > wheelPositions && videoId < wheelPositions * 2 + 1) {
+      console.log('suntem in sectiunea 14-28 si dupa ce se termina aceasta trebuie sa facem play la result')
+      playResultVideo();
+    } else if (videoId < wheelPositions + 1) {
+      if (firstSpin) {
+        setFirstSpin(false);
+      }
+      console.log('s-a terminat un start video asa ca trebuie sa pornim un stop video')
+      playStopVideo();
+    } else if (videoId > wheelPositions * 2) {
+      console.log('trebuie sa mai generam un nou numar random si sa facem play la un video din sectiunea 1-14')
+      // handlePlayVideo();
+    }
   };
+
+
+  const playStopVideo = () => {
+    console.log('here we play the stop  video');
+    console.log('previous videoId is, ', videoId);
+    if (videoRefs.current[videoId - 1]) {
+      videoRefs.current[videoId - 1].pause();
+      videoRefs.current[videoId - 1].currentTime = 0;
+    }
+
+    setVideoId(getRandomNumber(15, 28));
+    console.log('newVideoId is in playStopVideo ', videoId)
+    setIsPlaying(true);
+  }
+
+  const playResultVideo = () => {
+    console.log('here we play the result effect video');
+    console.log('previous videoId is, ', videoId);
+    if (videoRefs.current[videoId - 1]) {
+      videoRefs.current[videoId - 1].pause();
+      videoRefs.current[videoId - 1].currentTime = 0;
+    }
+
+    setVideoId(videoId + wheelPositions);
+    console.log('newVideoId is ', videoId)
+    setIsPlaying(true);
+  }
 
   return (
     <div className="md:absolute top-0 left-0 w-full md:h-full overflow-hidden -z-1 video-container">
       {isLoading ? (
         <div className="absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4">
-          <Loading />
+          <Loading/>
         </div>
       ) : (
         videoBlobs.length > 0 &&
@@ -123,7 +179,7 @@ const WheelContainer: React.FC = () => {
             onEnded={handleVideoEnd}
             loop={false}
             controls={false}
-            muted={true}
+            muted={false}
             playsInline
             className={`md:absolute w-full h-full object-cover ${
               videoId === index + 1 ? "block" : "hidden"
@@ -137,7 +193,7 @@ const WheelContainer: React.FC = () => {
           {videoId} : Balance: ${balance.toFixed(2)}
         </span>
       </div>
-      <Jackpot />
+      <Jackpot/>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
         {!isPlaying && !isLoading && (
           <Image
