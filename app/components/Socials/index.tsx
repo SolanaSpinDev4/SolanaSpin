1d972022d5ce5d525ef92b576fd80475ac00392a
SolanaSpinDev3 <dev_sol_99@proton.me>
2024-10-26 17:21:38 +0300

    social icons

--- app/components/Socials/index.tsx
+++ app/components/Socials/index.tsx
@@ -1,9 +1,14 @@
 import {PiTelegramLogoLight} from 'react-icons/pi';
 import { FaXTwitter } from "react-icons/fa6";
+import Link from 'next/link';
 
 export const Socials = () => {
   return (<div className="flex items-center space-x-4">
-    <PiTelegramLogoLight className="text-white text-xl lg:text-3xl"/>
-    <FaXTwitter className="text-white text-xl lg:text-3xl"/>
+    <Link href="https://t.me/solanaspin" target="_blank" rel="noopener noreferrer">
+      <PiTelegramLogoLight className="text-white text-xl lg:text-3xl"/>
+    </Link>
+   <Link href="" target="_blank" rel="noopener noreferrer">
+     <FaXTwitter className="text-white text-xl lg:text-3xl"/>
+   </Link>
   </div>)
 }
